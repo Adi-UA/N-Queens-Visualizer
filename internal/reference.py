53268603bcb4f2df24f4c7d04443c8e27f5262bf
adi-ua <abanerjee@email.arizona.edu>
2020-05-23 04:48:50 -0700

    autopep8 formatting

--- internal/reference.py
+++ internal/reference.py
@@ -3,10 +3,10 @@ import pygame
 # Common variables I'm using later
 
 pygame.init()
-WHITE_SQ = pygame.Surface((50,50))
-WHITE_SQ.fill((224,224,224))
-GREEN_SQ = pygame.Surface((50,50))
-GREEN_SQ.fill((153,255,51))
-RED_SQ = pygame.Surface((50,50))
-RED_SQ.fill((255,51,51))
+WHITE_SQ = pygame.Surface((50, 50))
+WHITE_SQ.fill((224, 224, 224))
+GREEN_SQ = pygame.Surface((50, 50))
+GREEN_SQ.fill((153, 255, 51))
+RED_SQ = pygame.Surface((50, 50))
+RED_SQ.fill((255, 51, 51))
 TILE_SIZE = 50
